99206766e471bbc4b2c350d183ef28261aff97b4
tsuki <106149353+cSuk1@users.noreply.github.com>
2022-11-21 23:05:00 +0800

    修改代理池

--- ua_info.py
+++ ua_info.py
@@ -8,4 +8,5 @@ ua_list = [
     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1',
     'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
 ]
